0564eda73d2687828c6f5059fb820061dd96dffd
Denis Bekkulov <dionisvegan@icloud.com>
2024-07-31 01:25:04 +0300

    make all features

--- src/App.js
+++ src/App.js
@@ -4,23 +4,63 @@ import styles from './App.module.css';
 const App = () => {
 	const [input, setInput] = useState('');
 
-	const NUMS = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+	const NUMS = [
+		'C',
+		'+',
+		'-',
+		'7',
+		'8',
+		'9',
+		'4',
+		'5',
+		'6',
+		'1',
+		'2',
+		'3',
+		'0',
+		'=',
+	];
+
+	const handleClick = value => {
+		setInput(input => input + value);
+	};
+
+	const calculate = () => {
+		setInput(eval(input));
+	};
+
+	const clear = () => {
+		setInput('');
+	};
 
 	return (
 		<div className={styles.container}>
-			<label htmlFor='calc' className={styles.calculatorLabel}>
-				ЧТО НУЖНО ПОСЧИТАТЬ?
-			</label>
-			<input type='text' id='calc' className={styles.calculatorInput} />
-
+			<input
+				type='text'
+				id='calc'
+				className={styles.calculatorInput}
+				value={input}
+			/>
 			<div className={styles.calculator}>
-				{NUMS.map((num, index) => (
+				{NUMS.map(value => (
 					<button
-						className={styles.numberButton}
-						key={num}
-						onClick={() => setInput(input => input + NUMS[index])}
+						key={value}
+						className={`${value === '0' && styles.zero} ${
+							value !== '0' && styles.btn
+						} ${value === '=' && styles.operation} ${
+							value === '-' && styles.operation
+						} ${value === '+' && styles.operation} ${
+							value === 'C' && styles.clear
+						}`}
+						onClick={() =>
+							`${value === '=' && calculate()} ${
+								value === '-' && handleClick(value)
+							} ${value === '+' && handleClick(value)} ${
+								value === 'C' && clear()
+							} ${Number(value) <= 9 && handleClick(value)}`
+						}
 					>
-						{num}
+						{value}
 					</button>
 				))}
 			</div>
